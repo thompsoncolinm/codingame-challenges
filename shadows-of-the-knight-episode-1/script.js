ffa1a37aa0f1323d20eafc919d3fadab9a476c65
Colin Thompson <thompsoncolinm@gmail.com>
2024-07-23 13:16:48 -0400

    3/4 tests passed

--- shadows-of-the-knight-episode-1/script.js
+++ shadows-of-the-knight-episode-1/script.js
@@ -11,14 +11,55 @@ var inputs = readline().split(' ');
 const X0 = parseInt(inputs[0]);
 const Y0 = parseInt(inputs[1]);
 
+let batPos = {
+    'x' : X0,
+    'y' : Y0
+}
+
 // game loop
 while (true) {
     const bombDir = readline(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+    moveDir = '';
+    
+
+    try {
+        console.error('Bomb Direction: ', bombDir);
+
+        if (bombDir.includes('R') && checkWidth(batPos.x + 1)){
+                moveDir += ++batPos.x + ' ';
+        } else if (bombDir.includes('L') && checkWidth(batPos.x - 1)){
+            moveDir += ++batPos.x + ' ';
+        } else {
+            moveDir += batPos.x + ' ';
+        }
+        if (bombDir.includes('U') && checkHeight(batPos.y - 1)){
+            moveDir += (--batPos.y).toString();
+        } else if (bombDir.includes('D') && checkHeight(batPos.y + 1)){
+            moveDir += (++batPos.y).toString();
+        } else {
+            moveDir += (batPos.y).toString();;
+        }
+    } catch(e) {
+        console.error('Something went wrong', e)
+    }
 
     // Write an action using console.log()
     // To debug: console.error('Debug messages...');
 
 
     // the location of the next window Batman should jump to.
-    console.log('0 0');
+    console.log(moveDir);
+}
+
+function checkWidth(batW){
+    if (batW < 0 || batW > W) {
+        return false;
+    }
+    return true;
+}
+function checkHeight(batH){
+    if (batH < 0 || batH > H) {
+        return false;
+    }
+    return true;
 }
